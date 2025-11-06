02ed055029ec15b6b3846bba23c0250d8efa5fe9
snake <GitHub0227@gmail.com>
2025-11-06 11:57:59 +0900

    Add a command

---
+++ test.bash
@@ -0,0 +1,24 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Daichi Utsugi
+# SPDX-License-Identifier: BSD-3-Clause
+ng(){
+        echo ${1}行目が違う
+	res=1 
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
